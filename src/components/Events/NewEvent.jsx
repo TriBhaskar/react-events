004ce8b287266b45a5ab186a88f060071e450bef
TriBhaskar <panthribhaskar@gmail.com>
2024-02-25 22:14:51 +0530

    Add mutation function and form submission handling to NewEvent component

--- src/components/Events/NewEvent.jsx
+++ src/components/Events/NewEvent.jsx
@@ -5,16 +5,20 @@ import EventForm from "./EventForm.jsx";
 import { useMutation } from "@tanstack/react-query";
 import { createNewEvent } from "../../util/http.js";
 
+// Define the NewEvent component
 export default function NewEvent() {
   const navigate = useNavigate();
 
   const { mutate } = useMutation({
     mutationFn: createNewEvent,
-  });
+  }); // Create a mutation function to create a new event
+
+  // Handle form submission
   function handleSubmit(formData) {
     mutate({ event: formData });
   }
 
+  // Render the NewEvent component
   return (
     <Modal onClose={() => navigate("../")}>
       <EventForm onSubmit={handleSubmit}>
