ffd18776782be5cc1a487054dd30bdd1c86d916b
TriBhaskar <panthribhaskar@gmail.com>
2024-02-25 22:19:16 +0530

    Add error handling and loading state to NewEvent component

--- src/components/Events/NewEvent.jsx
+++ src/components/Events/NewEvent.jsx
@@ -4,12 +4,13 @@ import Modal from "../UI/Modal.jsx";
 import EventForm from "./EventForm.jsx";
 import { useMutation } from "@tanstack/react-query";
 import { createNewEvent } from "../../util/http.js";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 // Define the NewEvent component
 export default function NewEvent() {
   const navigate = useNavigate();
 
-  const { mutate } = useMutation({
+  const { mutate, isPending, error, isError } = useMutation({
     mutationFn: createNewEvent,
   }); // Create a mutation function to create a new event
 
@@ -22,15 +23,28 @@ export default function NewEvent() {
   return (
     <Modal onClose={() => navigate("../")}>
       <EventForm onSubmit={handleSubmit}>
-        <>
-          <Link to="../" className="button-text">
-            Cancel
-          </Link>
-          <button type="submit" className="button">
-            Create
-          </button>
-        </>
+        {isPending && "Submitting..."}
+
+        {!isPending && (
+          <>
+            <Link to="../" className="button-text">
+              Cancel
+            </Link>
+            <button type="submit" className="button">
+              Create
+            </button>
+          </>
+        )}
       </EventForm>
+      {isError && (
+        <ErrorBlock
+          title="Failed to create event"
+          message={
+            error.info?.message ||
+            "Failed to create event. Please check your inputs and try again later"
+          }
+        />
+      )}
     </Modal>
   );
 }
