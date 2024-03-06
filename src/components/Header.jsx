cab5c018f3ee7794c55037c4d352569c30c39fa1
TriBhaskar <panthribhaskar@gmail.com>
2024-03-06 20:15:57 +0530

    Add progress bar for ongoing fetch requests in Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,16 @@
 import { useIsFetching } from "@tanstack/react-query";
 
+// Header component
 export default function Header({ children }) {
+  // Get the number of ongoing fetch requests
   const fetching = useIsFetching();
+
   return (
     <>
+      {/* Show a progress bar if there are ongoing fetch requests */}
       <div id="main-header-loading">{fetching > 0 && <progress />}</div>
+
+      {/* Main header */}
       <header id="main-header">
         <div id="header-title">
           <h1>React Events</h1>
